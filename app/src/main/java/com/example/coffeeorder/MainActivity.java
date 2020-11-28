697d5b75a477d2df13acf68937b532629611006b
imanshaterian <imanshaterian@gmail.com>
2020-11-28 14:05:13 +0330

    created the foundation of app

--- app/src/main/java/com/example/coffeeorder/MainActivity.java
+++ app/src/main/java/com/example/coffeeorder/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.coffeeorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //variables:
+    int quantity = 0;
+//    String name = "";
+//    boolean chocolate = false;
+//    boolean whippedCream = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void increment(View v) {
+        quantity++;
+        displayQuantity(quantity);
+    }
+
+    public void decrement(View v) {
+        quantity = quantity - 1;
+        displayQuantity(quantity);
+    }
+
+    public void displayQuantity(int number) {
+        TextView displayInteger = findViewById(R.id.quantityNumber);
+        displayInteger.setText(number);
+    }
+
 }
